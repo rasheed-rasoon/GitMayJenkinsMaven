0b2697073bbfec5f63f52192039933f24b9163b7
rasheed-rasoon <rasheed.rasoon@gmail.com>
2019-04-28 23:05:18 +0530

    commits in May

---
+++ MavenJenkinsGitHubInMay/src/test/java/MavenJenkinsGitHubInMay/MavenJenkinsGitHubInMay/SampleTest.java
@@ -0,0 +1,10 @@
+package MavenJenkinsGitHubInMay.MavenJenkinsGitHubInMay;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+  @Test
+  public void sample() {
+	  System.out.println("maven integrated with jenkins , Working fine");
+  }
+}
