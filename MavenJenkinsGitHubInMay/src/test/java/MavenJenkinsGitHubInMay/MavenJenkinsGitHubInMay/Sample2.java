339f79e6f7cefc3e6cb9ffada73e97211111ffe7
rasheed-rasoon <rasheed.rasoon@gmail.com>
2019-04-28 23:17:01 +0530

    second commit

---
+++ MavenJenkinsGitHubInMay/src/test/java/MavenJenkinsGitHubInMay/MavenJenkinsGitHubInMay/Sample2.java
@@ -0,0 +1,10 @@
+package MavenJenkinsGitHubInMay.MavenJenkinsGitHubInMay;
+
+import org.testng.annotations.Test;
+
+public class Sample2 {
+  @Test
+  public void Sample2() {
+	  System.out.println("Second commit");
+  }
+}
